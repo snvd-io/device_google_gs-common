9aa2c2e0017a8500b21ebdf1105e4f3b9dd66139
Hongyang Jiao <jiaohy@google.com>
2024-02-09 21:28:46 +0000

    Add betterbug folder to gs-common
    Test: locally test, recorded in http://shortn/_iEz9DTCHgo
    Bug: 322543833
    Change-Id: I035adde2253056414d7c729f4a9f5b4effdde87d

---
+++ betterbug/betterbug.mk
@@ -0,0 +1,5 @@
+PRODUCT_PACKAGES += BetterBugStub
+PRODUCT_PACKAGES_DEBUG += BetterBug
+
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/gs-common/betterbug/sepolicy/product/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/gs-common/betterbug/sepolicy/product/private
