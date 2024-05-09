99f848ed486b3698db398e63fb1613b9c17ec1b3
Kelvin Zhang <zhangkelvin@google.com>
2024-05-09 16:45:02 +0000

    Add a different implementation of insmod.sh
    Use soong namespace to branch a implementation of insmod.sh .
    The newly added implementation is only added to PRODUCT_PACKAGE
    for dev option enabled devices. The only change to the script is
    skip loading kernel modules from vendor|system_dlkm when under 16K
    mode.
    
    Test: Dev option works
    Bug: 319335586
    
    This reverts commit 11eb02562d7e89037c67f5a22a142b110b303a30.
    
    Change-Id: I5f5d4ee79be7f872ec321e93f58f6516974d0cad

--- insmod/insmod.mk
+++ insmod/insmod.mk
@@ -1,3 +1,9 @@
+ifeq (true,$(PRODUCT_16K_DEVELOPER_OPTION))
+PRODUCT_SOONG_NAMESPACES += device/google/gs-common/insmod/16k
+else
+PRODUCT_SOONG_NAMESPACES += device/google/gs-common/insmod/4k
+endif
+
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/insmod/sepolicy
 PRODUCT_PACKAGES += \
         insmod.sh \
