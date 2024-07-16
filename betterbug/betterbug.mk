a0e5b359369c9f706a79ba4c4531f0216437ae5c
Hongyang Jiao <jiaohy@google.com>
2024-07-16 23:09:21 +0000

    Apply flag RELEASE_PACKAGE_BETTER_BUG
    Flag: build.RELEASE_PACKAGE_BETTER_BUG
    Bug: 353584670
    Change-Id: I41b3fe23f68d46d29ef12692d00e1c6c1f7f141f
    Test: manual test

--- betterbug/betterbug.mk
+++ betterbug/betterbug.mk
@@ -1,7 +1,7 @@
 # When neither AOSP nor factory targets
 ifeq (,$(filter aosp_% factory_%, $(TARGET_PRODUCT)))
   PRODUCT_PACKAGES += BetterBugStub
-  PRODUCT_PACKAGES_DEBUG += BetterBug
+  PRODUCT_PACKAGES_DEBUG += $(RELEASE_PACKAGE_BETTER_BUG)
 endif
 
 PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/gs-common/betterbug/sepolicy/product/public
