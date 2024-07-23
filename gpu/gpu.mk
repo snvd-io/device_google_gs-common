6d8cc33ddeeec518b7c99b20b86a2a42ddeb7273
Devika Krishnadas <kdevika@google.com>
2024-07-23 21:11:26 +0000

    Rename arm-gralloc namespace to pixel-gralloc for mapper-version
    Bug: 355046697
    Test: Build and boot
    Change-Id: Ic4e7bf898aec3bf37fe3b46d5109d06f86b13646
    Signed-off-by: Devika Krishnadas <kdevika@google.com>

--- gpu/gpu.mk
+++ gpu/gpu.mk
@@ -8,9 +8,9 @@ PRODUCT_PACKAGES += pixel_gralloc_allocator
 PRODUCT_PACKAGES += pixel_gralloc_mapper
 
 ifeq ($(USE_MAPPER5), true)
-$(call soong_config_set,arm_gralloc,mapper_version,mapper5)
+$(call soong_config_set,pixel_gralloc,mapper_version,mapper5)
 $(call soong_config_set,aion_buffer,mapper_version,mapper5)
 else
-$(call soong_config_set,arm_gralloc,mapper_version,mapper4)
+$(call soong_config_set,pixel_gralloc,mapper_version,mapper4)
 $(call soong_config_set,aion_buffer,mapper_version,mapper4)
 endif
