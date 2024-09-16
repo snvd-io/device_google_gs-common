d43a6e1c5a677614ff924bf9b47ed165e908a4de
Priyanka Advani (xWF) <padvani@google.com>
2024-09-16 16:58:16 +0000

    Revert^2 "Allow devices that use HIDL to find AIDL radio_ext_service"
    This reverts commit 1685969e605263ad55f1f75e50584dc0f12bd99a.
    
    Reason for revert: Droidmonitor created revert due to b/367330939.
    
    Change-Id: Idd70cf3d846fad1a25060ebfb6ae6a99599fd861

--- gril/hidl/1.7/gril_hidl.mk
+++ gril/hidl/1.7/gril_hidl.mk
@@ -1,3 +1,4 @@
 PRODUCT_PACKAGES += vendor.google.radioext@1.0-service
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/gril/hidl/1.7/compatibility_matrix.xml
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/gril/hidl/1.7/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/gril/common/sepolicy
--- modem/radio_ext/radio_ext.mk
+++ modem/radio_ext/radio_ext.mk
@@ -3,3 +3,4 @@ PRODUCT_PACKAGES += vendor.google.radio_ext-service
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/modem/radio_ext/compatibility_matrix.xml
 
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/modem/radio_ext/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/gril/common/sepolicy
